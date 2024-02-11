3f08fa6d1032fe0cc605bbb417ead82dbe270940
ransela <62696300+ransela@users.noreply.github.com>
2024-02-11 14:44:56 +1000

    Initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup>
+const pages = ['/', '/hello', '/world', '/vue'];
+</script>
+
+<template>
+  <div>
+    <NuxtLoadingBar :duration="1000" />
+    <ul>
+      <li v-for="page of pages">
+        <NuxtLink :to="page">{{ page }}</NuxtLink>
+      </li>
+    </ul>
+    <NuxtPage />
+  </div>
+</template>
---
+++ plugins/loading-bar.client.ts
@@ -0,0 +1,12 @@
+// import { defineNuxtPlugin } from '#imports';
+
+export default defineNuxtPlugin(() => {
+  const app = useNuxtApp();
+
+  app.hook('page:start', () => {
+    console.log('Page start...');
+  });
+  app.hook('page:finish', () => {
+    console.log('Page finished!');
+  });
+});
